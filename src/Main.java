778a812300e545f30f5ca2a1a0107557d476cd6c
Denis <protsdv@gmail.com>
2022-12-20 14:14:33 +0300

    Question 3. Work is done.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Arrays;
+
 public class Main {
+    ///////Напишите программу, которая переворачивает массив.
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] array = {4, 8, 1, 7, 3};
+        for (int i = 0; i <= array.length % 2; i++) {
+            int temp = array[array.length - 1 - i];
+            array[array.length - 1 - i] = array[i];
+            array[i] = temp;
+        }
+        System.out.println(Arrays.toString(array));
     }
 }
